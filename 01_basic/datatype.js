a2370f9f8bbfba7ea8cc259526a645037f99fe7f
Ankit Sha <108385991+ankitsha093@users.noreply.github.com>
2024-08-13 17:35:30 +0000

    datatype done

---
+++ 01_basic/datatype.js
@@ -0,0 +1,2 @@
+"use strict" //Use all Js code as newes verson 
+alert(3+3) //we are using node js not browser
---
+++ 01_basic/var.js
@@ -0,0 +1,12 @@
+const accID = 12354;
+let accEmail = "anki@gmail.com";
+var accCity = "delhi"
+
+let accState
+
+console.table([accID,accEmail,accCity]);
+
+
+accEmail = "kk@gmail.com"
+accCity = "Dubai"
+console.table([accID,accEmail,accCity,accState]);
