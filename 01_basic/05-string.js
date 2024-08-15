32d74b885dd710890d9ebc8969259773d31f9fee
Ankit Sha <108385991+ankitsha093@users.noreply.github.com>
2024-08-15 08:25:32 +0000

    string completed

---
+++ 01_basic/05-string.js
@@ -0,0 +1,18 @@
+// string methods 
+
+const fullNaMe = "ankit is king don no 1";
+const fullNaMe2 = "ankit is king don no 1";
+
+const fullNameArr = fullNaMe.split(" ");
+
+// console.log(fullNameArr) (it will give you new array removing space)
+
+// isName = fullNaMe.includes("aman") (if text is available in line it will return true)
+
+if(isName){
+    console.log(`Yes is indludes in this line`)
+}
+else{
+    console.log(`no, "is" not includes in this line`)
+    
+}
--- 01_basic/datatype-summy.js
+++ 01_basic/datatype-summy.js
@@ -2,8 +2,41 @@ let ankit = null;
 // console.log(typeof(ankit))
 // will show type object
 
-let aman = Symbol(123)
-let ankit2 = Symbol(123)
+// let aman = Symbol(123)
+// let ankit2 = Symbol(123)
 
-console.log(aman)
-console.log(ankit2)
+// console.log(aman)
+// console.log(ankit2)
+
+
+
+// -------------------
+
+
+// Memory 
+// Stack (Primitive) and Heap (non-primitive )
+
+
+// data type primitive goes to Stack Memory and no pimitive goes to Heap  Memory
+// stack will give you a copy of data but Heap will give a reference data (Example :v)
+
+
+ let user = {
+    name: "ankit",
+    email: "ank@gmail.com"
+}
+
+let user2 = user;
+// it will show ankit name as it is given in user1
+
+
+user2.name = "aman";
+
+
+// it will chagne original object because it is a reference data (not a copy of original data)
+
+console.log(user.name);
+console.log(user2.name);
+
+// in both condition it will "aman" because it has change the original value
+
