5cfc475a6f6a28f5b56b1cd2d9d1484be9a65d7f
Ankit Sha <108385991+ankitsha093@users.noreply.github.com>
2024-08-18 18:06:42 +0000

    functin basic

---
+++ 03_basic/function.js
@@ -0,0 +1,18 @@
+function add(num1,num2){
+
+
+    // wehn we create a function the value between kk() called paramenter
+if(num1){
+
+    return num1+num2
+
+}
+
+console.log("Pls Enter Number")
+
+}
+
+const finalVal = add(52,25); //When We Call Function the value between () call argument 
+
+
+console.log(add(""));
