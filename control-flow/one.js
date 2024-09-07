a5a1405060b9f8eb2f70d660682839b6c9ffc6e8
Ankit Sha <108385991+ankitsha093@users.noreply.github.com>
2024-09-07 09:32:57 +0000

    Truth and Falcy, check object empth or not

---
+++ control-flow/one.js
@@ -0,0 +1,50 @@
+// Truty and Falsy Value 
+
+
+// When we Assume that value would be true and false  example
+
+const className = "five"
+
+if(className){
+
+    //in THis is case we assume that "classname have a true value"
+console.log('we have class name');
+}
+
+else{
+    console.log(`we don't have class`)
+}
+
+
+// Falsy Values Are = 
+
+// false, 0, -0, BigInt, 0n, "0", null, undefined, Nan // These are falsy value (All value except these are Truthy Value)
+
+// Surprise (truthy value) valie 
+
+// "0",'false', ' ',[],{},function(){}  (All inside quotes will be truty value )
+
+
+// check array is  empty
+
+const users = []
+
+if(users.length === 0){
+    console.log("array is empty")
+}
+
+// check object is empty or not
+
+const myObj = {
+    user: "ankit",
+    gender : "male"
+};
+
+
+console.log(Object.keys(myObj))
+if(Object.keys(myObj).length===0){
+    console.log(`object is Empty`)
+}
+else{
+    console.log("array is not empty")
+}
