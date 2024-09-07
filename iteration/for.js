e177955cfe8601c7280006ad65d2a779c8a73155
Ankit Sha <108385991+ankitsha093@users.noreply.github.com>
2024-09-07 18:24:15 +0000

    break and continue in for loop

--- iteration/for.js
+++ iteration/for.js
@@ -11,13 +11,44 @@ for (let index = 0; index < 10; index++) {
 
 // For Practice Print Table
 
-let table=1
-for(table;table<=10;table++){
-    let ulti=1
-for(ulti;ulti<=10;ulti++){
+// let table=1
+// for(table;table<=10;table++){
+//     let ulti=1
+// for(ulti;ulti<=10;ulti++){
 
 
-    console.log(`${table} * ${ulti} = ${table*ulti}`)
-}
-}
+//     console.log(`${table} * ${ulti} = ${table*ulti}`)
+// }
+// }
 
+// 
+
+// Break and continue in for loop 
+
+
+// for (let index = 0; index <= 20; index++) {
+
+// if(index==5){
+//     console.log(`Number 5 is Detected`)
+//     // Break Use karte hi aage ke code ko stop kar deta hai
+//     break
+// }
+
+//     console.log(index)
+    
+// }
+
+
+for (let index1 = 0; index1 <= 20; index1++) {
+
+    if(index1==5){
+        console.log(`Number 5 is Detected`)
+        // continue use krte hi vo ek bar loop escape kr dega (just for one time)
+        continue
+    }
+    
+        console.log(index1) //output will be (1,2,3,4,)6,7,8,9,10) //it will skip number 5 
+        
+    }
+    
+    
